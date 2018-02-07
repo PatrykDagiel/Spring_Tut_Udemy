4dcbba765f109f6705cd1c6cc19c5a9169be2f26
dagiel <patryk.dagiel@gmail.com>
2018-02-07 09:09:32 +0100

    10th update

--- src/main/java/spring/tutorial/spring_udemy/BeanScopeDemoApp.java
+++ src/main/java/spring/tutorial/spring_udemy/BeanScopeDemoApp.java
@@ -11,5 +11,15 @@ public class BeanScopeDemoApp {
         //retrieve bean from spring container
         Coach theCoach = context.getBean("myCoach", Coach.class);
         Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        //check if they are same
+        boolean result = (theCoach == alphaCoach);
+
+        //print out the results
+        System.out.println("\nPointing to same object: " + result);
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+
+        context.close();
     }
 }
