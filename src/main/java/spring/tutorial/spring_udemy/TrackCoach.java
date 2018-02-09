5c22acc1d0f0d4cd3093c69d3f51fc01b89447f4
dagiel <patryk.dagiel@gmail.com>
2018-02-09 12:29:53 +0100

    11th update

--- src/main/java/spring/tutorial/spring_udemy/TrackCoach.java
+++ src/main/java/spring/tutorial/spring_udemy/TrackCoach.java
@@ -12,6 +12,16 @@ public class TrackCoach implements Coach {
 
     }
 
+    // add an init method
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+
+    // add a destroy method
+    public void doMyCleanupStuffYoYo() {
+        System.out.println("TrackCoach: inside method doMyCleanup");
+    }
+
     @Override
     public String getDailyWorkout()
     {
