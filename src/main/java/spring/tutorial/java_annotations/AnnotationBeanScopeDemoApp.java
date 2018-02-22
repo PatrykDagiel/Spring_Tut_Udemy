1ae516b1dffd7967ac3a23771e17697c151d4dc2
dagiel <patryk.dagiel@gmail.com>
2018-02-22 10:05:42 +0100

    20th Update, Scope with annotations

---
+++ src/main/java/spring/tutorial/java_annotations/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,24 @@
+package spring.tutorial.java_annotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+    public static void main(String[] args) {
+
+        //load spring config file
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_java.xml");
+
+        // retrieve some bean from container
+
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+        boolean areEqual = (theCoach == alphaCoach);
+        System.out.println("\nPointing to the same object: " + areEqual);
+        System.out.println("\nMemory location for theCoach " + theCoach);
+        System.out.println("\nMemory location for alphaCoach " + alphaCoach);
+
+        context.close();
+    }
+}
