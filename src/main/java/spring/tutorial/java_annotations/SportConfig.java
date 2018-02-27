28302b087575bb62dc02cef5393b7edd9b48af89
Patryk Dagiel <patryk.dagiel@gmail.com>
2018-02-27 21:39:26 +0100

    Update 27-02-2018 v2, Java-based APP configuration

---
+++ src/main/java/spring/tutorial/java_annotations/SportConfig.java
@@ -0,0 +1,10 @@
+package spring.tutorial.java_annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring.tutorial.java_annotations")
+public class SportConfig {
+
+}
--- src/main/java/spring/tutorial/java_annotations/TennisCoach.java
+++ src/main/java/spring/tutorial/java_annotations/TennisCoach.java
@@ -4,6 +4,7 @@ import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
