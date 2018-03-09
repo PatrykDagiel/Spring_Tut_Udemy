af5d3682fa19167f4280a863b3018525acd5bbfb
dagiel <patryk.dagiel@gmail.com>
2018-03-09 11:43:09 +0100

    21st Update, Start Java annotations (SportConfig) + SwimCoach Class

--- src/main/java/spring/tutorial/java_annotations/SportConfig.java
+++ src/main/java/spring/tutorial/java_annotations/SportConfig.java
@@ -1,10 +1,22 @@
 package spring.tutorial.java_annotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("spring.tutorial.java_annotations")
 public class SportConfig {
+
+    @Bean
+    public Coach swimCoach(FortuneService fortuneService) {
+        SwimCoach mySwimCoach = new SwimCoach(happyFortuneService());
+        return mySwimCoach;
+    }
+
+    @Bean
+    public FortuneService happyFortuneService() {
+        return new HappyFortuneService();
+    }
 
 }
---
+++ src/main/java/spring/tutorial/java_annotations/SwimCoach.java
@@ -0,0 +1,7 @@
+package spring.tutorial.java_annotations;
+
+public class SwimCoach implements Coach {
+    public SwimCoach(FortuneService fortuneService) {
+
+    }
+}
