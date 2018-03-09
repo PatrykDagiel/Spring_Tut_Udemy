667cf88ca19c3dce8d5beb640e25242dd29f28ea
dagiel <patryk.dagiel@gmail.com>
2018-03-09 11:52:31 +0100

    Video 88 finished

--- src/main/java/spring/tutorial/java_annotations/SwimCoach.java
+++ src/main/java/spring/tutorial/java_annotations/SwimCoach.java
@@ -1,7 +1,20 @@
 package spring.tutorial.java_annotations;
 
 public class SwimCoach implements Coach {
-    public SwimCoach(FortuneService fortuneService) {
 
+    private FortuneService fortuneService;
+
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000m as a warmup";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
--- src/main/java/spring/tutorial/spring_udemy/BeanLifeCycleDemoApp.java
+++ src/main/java/spring/tutorial/spring_udemy/BeanLifeCycleDemoApp.java
@@ -7,7 +7,6 @@ public class BeanLifeCycleDemoApp {
     public static void main(String[] args) {
         //load the Spring configuration file
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
-
         //retrieve bean from spring container
         Coach theCoach = context.getBean("myCoach", Coach.class);
         Coach alphaCoach = context.getBean("myCoach", Coach.class);
