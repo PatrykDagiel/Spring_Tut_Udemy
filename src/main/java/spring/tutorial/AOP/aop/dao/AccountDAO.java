bbc794dd368ede46954c90c03ac2fb7229d9bebe
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-09-19 15:55:58 +0200

    N/A / cosmetic

--- src/main/java/spring/tutorial/AOP/aop/dao/AccountDAO.java
+++ src/main/java/spring/tutorial/AOP/aop/dao/AccountDAO.java
@@ -2,4 +2,9 @@ package spring.tutorial.AOP.aop.dao;
 
 
 public class AccountDAO {
+
+    public void addAccount() {
+        System.out.println(getClass() + " : doing my DB work: Adding and account");
+    }
+
 }
