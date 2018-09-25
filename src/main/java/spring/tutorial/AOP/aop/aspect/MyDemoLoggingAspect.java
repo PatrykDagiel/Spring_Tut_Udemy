e5ee1d898a7cbbe88c511a20531ce6a1f7c730f2
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-09-25 12:30:46 +0200

    Past 341.

--- src/main/java/spring/tutorial/AOP/aop/aspect/MyDemoLoggingAspect.java
+++ src/main/java/spring/tutorial/AOP/aop/aspect/MyDemoLoggingAspect.java
@@ -2,17 +2,28 @@ package spring.tutorial.AOP.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+
+    @Pointcut("execution(* spring.tutorial.AOP.aop.dao.*.*(..))")
+    private void forDoPackage() {}
+
     // this is where we add all of our related advices for logging
 
     // let's start with an @before advice
 //    @Before("execution(* add*(spring.tutorial.AOP.aop.demo.Account, ..))")
-    @Before("execution(* spring.tutorial.AOP.aop.dao.*.*(..))")
+    @Before("forDoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.printf("\n==========> Executing @Before advice on addAccount()");
     }
+
+    @Before("forDoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n======>>> Perform API analytics");
+    }
+
 }
